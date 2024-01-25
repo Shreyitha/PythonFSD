907a4666d219dc1d9584a999a9369333ae76b560
Sai Pradeep Reddy <92703130+20Pradeepreddy@users.noreply.github.com>
2024-01-25 08:38:44 +0530

    Add files via upload

---
+++ activity2/pradeep_activity2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[21]:
+
+
+import getpass
+
+class PasswordManager:
+    def get_password(self):
+        try:
+            password = getpass.getpass("Enter your password: ")
+            return password
+
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+            
+password_manager = PasswordManager()
+
+try:
+    user_password = password_manager.get_password()
+    print("Password entered:********** ")
+
+except KeyboardInterrupt:
+    print("\nPassword input was interrupted.")
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
+
