c633ab0bb0716225faacc16d8d128ce529c77d5a
Saksha K Bangera <sakshabangera0133@gmail.com>
2024-01-29 11:13:37 +0530

    Add files via upload

---
+++ activity4_saksha/client.py
@@ -0,0 +1,25 @@
+import socket
+
+def main():
+    host = socket.gethostname()
+    port = 4000
+
+    client_socket = socket.socket()
+    client_socket.connect((host, port))
+
+    message = input("[Client] Enter a Message to Send to Server: ")
+
+    while True:
+        client_socket.send(message.encode())
+        data = client_socket.recv(512).decode()
+
+        print("[Client] Message Received from Server:", str(data))
+        message = input("[Client] Enter a Message to Send to Server: ")
+
+        if message == "quit":
+            break
+        
+    client_socket.close()
+        
+if __name__ == "__main__":
+    main()
