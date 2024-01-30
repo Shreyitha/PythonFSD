2765dfb72f4c6230c60641b513b15de9d8a5670e
Shreya <88547208+shreyasherigar@users.noreply.github.com>
2024-01-30 09:48:33 +0530

    added Activity5

---
+++ activity5/Shreya_activity5/app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request,redirect,url_for, session, flash
+
+app = Flask(__name__)
+app.secret_key = 'mysecretkey'
+
+@app.route("/", methods=["GET"])
+def login():
+    return render_template("login.html")
+
+@app.route("/login-submit", methods=["post"])
+def loginSubmit():
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    if username== 'admin' and password == 'admin':
+        flash('Login successful')
+        session['logged_in_user']='admin'
+        return redirect(url_for('dashboard')) 
+    else:
+        return render_template("login.html", error =True)
+
+@app.route("/dashboard")
+def dashboard():
+    
+    if 'logged_in_user' in session and session['logged_in_user']:
+        username = session['logged_in_user']
+    else:
+        return redirect(url_for("login"))
+
+    return render_template("dashboard.html", username=username)
+
+@app.route("/logout")
+def logout():
+    flash('you have been logged out')
+    session.pop('logged_in_user',None)
+    return redirect(url_for('login'))
+
+if __name__=='__main__':
+    app.run(debug=True)
