e7971774d7029050217999401904284cdbb4b327
Puli <66771051+pulitharun@users.noreply.github.com>
2024-01-31 07:09:01 +0530

    Update pulitharun_activity6.py

--- activity6/pulitharun_activity6/pulitharun_activity6.py
+++ activity6/pulitharun_activity6/pulitharun_activity6.py
@@ -12,7 +12,7 @@ if r.status_code == 200:
     for book in Books: 
         title = book.find('h3').find('a')['title']
         price = book.find(class_='price_color').get_text().replace("£", "")
-        books.append({'Title': title, 'Price': price}) #
+        books.append({'Title': title, 'Price': price}) 
     with open("bestselling.csv", "w", encoding="UTF-8", newline="") as csv_file: 
         csv_writer = csv.writer(csv_file)
         csv_writer.writerow(['Title', 'Price'])
