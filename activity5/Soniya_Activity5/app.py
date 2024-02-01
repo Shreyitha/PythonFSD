689ab547c6e68f0090a0b5f1ca1568ef582b78a0
soniya-jain12 <157033542+soniya-jain12@users.noreply.github.com>
2024-02-01 22:59:25 +0530

    Add files via upload

---
+++ activity5/Soniya_Activity5/app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, session
+
+app = Flask(__name__)
+app.secret_key = "your_secret_key"
+
+@app.route('/login', methods=['GET'])
+def login():
+    return render_template("login.html")
+
+@app.route('/login-submit', methods=['POST'])
+def loginSubmit():
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    if(username == 'admin' and password == 'admin'):
+        flash("Login Successful")
+        session['log_user']= username
+        return redirect(url_for('dashboard'))
+    else:
+        
+        return render_template("login.html", error = True)
+        
+    
+
+@app.route("/dashboard")
+def dashboard():
+    
+    if 'log_user' in session and session['log_user']:
+        username = session['log_user']
+    else:
+        return redirect(url_for("login"))
+
+    return render_template("dashboard.html", username=username)
+
+@app.route("/logout")
+def logout():
+    #flash('you have been logged out')
+    session.pop('log_user',None)
+    return redirect(url_for('login'))
+
+if __name__=='__main__':
+    app.run(debug=True)
